9bd0298f347a321c883e5482ead47fccb71c73ac
 yangtao <yt5538528@163.com>
2017-01-06 00:16:06 +0800

    add functions

--- examples/tools_test.php
+++ examples/tools_test.php
@@ -3,10 +3,8 @@
  * Created by PhpStorm.
  * User: yang
  * Date: 2017/1/3
- * Time: 15:36
  */
 require 'vendor/autoload.php';
-use \HappyNoion\Tool\Tools;
 
 $t = new Tools();
 echo $t->getDateTime();
