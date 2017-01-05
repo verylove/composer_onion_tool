02892ca0df3c79fbbf6104e5fca8652c10f734c7
Yang Tao <yt5538528@163.com>
2017-01-05 14:12:51 +0800

    add PHPExcel

---
+++ src/HappyOion/Tool/Tools.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yang
+ * Date: 2017/1/3
+ * Time: 15:30
+ */
+
+namespace HappyNoion\Tool;
+
+class Tools{
+
+    public  function __construct(){
+
+    }
+
+    public function getDateTime($format = "Y-m-d H:i:s"){
+        if($format == "") return 'error';
+        return date($format,time());
+    }
+
+    public function getPhpinfo(){
+        return phpinfo();
+    }
+
+}
